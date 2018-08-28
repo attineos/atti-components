8e51acda30c05689a5b7abf6bfd4f1938da55999
Atrakeur <atrakeur+gitlab@gmail.com>
2018-08-28 09:54:56 +0000

    Merge branch 'feature/theme' into 'develop'
    Feature/theme
    
    See merge request attineos/atti-components!20

---
+++ packages/atti-components-2/src/theme/colors.js
@@ -0,0 +1,13 @@
+/**
+ * Base colors.
+ */
+const colors = {
+  primary: 'green',
+  secondary: 'pink',
+  grey: 'grey',
+  black: 'black',
+  white: 'white',
+  transparent: 'transparent',
+}
+
+export default colors

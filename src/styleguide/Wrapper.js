b1ecf560de42b46fefba2ba066fd8fd56dd4931b
Victor Gosse <vgosse@mailjet.com>
2018-08-29 09:15:40 +0200

    Tech #37 - Update lint rules + fix corresponding issues

--- src/styleguide/Wrapper.js
+++ src/styleguide/Wrapper.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 import theme from '../theme'
 
 import ThemeProvider from '../components/ThemeProvider'
 
 class Wrapper extends React.Component {
+  static propTypes = {
+    /**
+     * The child node
+     */
+    children: PropTypes.node.isRequired,
+  }
+
   render() {
     const { children } = this.props
 

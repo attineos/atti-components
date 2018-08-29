840850b473d9841ec3ad7998682dc8f9cea96ddc
Atrakeur <atrakeur+gitlab@gmail.com>
2018-08-29 07:53:08 +0000

    Merge branch 'feature/remove_built_files' into 'develop'
    feature: Remove built files
    
    See merge request attineos/atti-components!26

--- packages/atti-components-2/lib/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var _Link = _interopRequireDefault(require("./components/Link"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-module.exports = {
-  Link: _Link.default
-};

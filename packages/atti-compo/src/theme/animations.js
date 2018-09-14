d088638ddc9093262117f4a3478bd0d26044b230
totocap <capettho@gmail.com>
2018-09-14 07:31:47 +0000

    Merge branch 'feature/13_loader' into 'develop'
    Feature/13 loader
    
    Closes #13
    
    See merge request attineos/atti-components!44

---
+++ packages/atti-compo/src/theme/animations.js
@@ -0,0 +1,30 @@
+/**
+ * Different animations values.
+ */
+const animations = {
+  duration: {
+    d0: '0s',
+    d1: '50ms',
+    d2: '100ms',
+    d3: '200ms',
+    d4: '400ms',
+    d5: '600ms',
+    d6: '800ms',
+    d7: '1s',
+    d8: '1.25s',
+    d9: '1.5s',
+    d10: '1.75s',
+    d11: '2s',
+    d12: '2.5s',
+    d13: '5s',
+  },
+  type: {
+    linear: 'linear',
+    ease: 'ease',
+    easeIn: 'ease-in',
+    easeOut: 'ease-out',
+    easeInOut: 'ease-in-out',
+  },
+}
+
+export default animations

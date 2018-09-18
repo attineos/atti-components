d8b7a77db2891aff05bc66efb549f8e323d4d485
Victor Gosse <v.gosse@attineos.com>
2018-09-18 12:56:17 +0200

    Some formatting on bin files + fix import on Text compo

--- native/TextNav.js
+++ native/TextNav.js
@@ -2,32 +2,32 @@ import React from 'react'
 import Text from '../src/components/Text'
 
 class TextNav extends React.Component {
-	
-	render8h4cckaioxo0 = () => {
-		const Text = require('react-native').Text
-		return (
-			<Text>Text :</Text>
-		)
-	}
+  
+  rendercqd0k1etq740 = () => {
+    const Text = require('react-native').Text
+    return (
+      <Text>Text :</Text>
+    )
+  }
 
-	render60jdvhsip7s0 = () => {
-		
-		return (
-			<Text>This is a Text</Text>
-		)
-	}
+  rendervpzlygomsr40 = () => {
+    
+    return (
+      <Text>This is a Text</Text>
+    )
+  }
 
-	
-	
-	render() {
-		return (
-			<React.Fragment>
-			
-{this.render8h4cckaioxo0()}
-{this.render60jdvhsip7s0()}
-			</React.Fragment>
-		)
-	}
+
+
+  render() {
+    return (
+      <React.Fragment>
+      
+{this.rendercqd0k1etq740()}
+{this.rendervpzlygomsr40()}
+      </React.Fragment>
+    )
+  }
 
 }
 

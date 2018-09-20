ab79f902cc34d9a2b3c2449222ddb1b3a6065be7
Victor Gosse <v.gosse@attineos.com>
2018-09-20 08:56:31 +0200

    PoC native exports - add theme for mobile and web

--- src/components/Button/styles/StyledButton.native.js
+++ src/components/Button/styles/StyledButton.native.js
@@ -2,6 +2,8 @@ import styled from 'styled-components'
 // eslint-disable-next-line
 import Button from 'react-native'
 
-const StyledButton = styled.Button``
+const StyledButton = styled.Button.attrs({
+  color: props => props.theme.components.button.colors.text,
+})``
 
 export default StyledButton

8354d1824a6f641fac5672f5fdf5da717b30be06
Clément ALLAIN <clement.allain@ymail.com>
2018-09-30 12:01:50 +0000

    Merge branch 'feature/44_label_component' into 'develop'
    Feature #44 - Label component
    
    Closes #44
    
    See merge request attineos/atti-components!64

---
+++ packages/atti-compo/src/components/Label/theme/index.js
@@ -0,0 +1,11 @@
+export default ({ colors, fonts }) => ({
+  colors: {
+    text: colors.black,
+  },
+  fonts: {
+    fontFamily: fonts.global.fontFamilyPrimary,
+    fontSize: fonts.fontSize.fs14,
+    fontWeight: fonts.fontWeight.fw600,
+    lineHeight: fonts.lineHeight.lh24,
+  },
+})

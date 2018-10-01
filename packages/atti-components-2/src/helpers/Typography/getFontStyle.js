05705ac19ff4d6832871b55daa48bb1f19fea9a1
Victor Gosse <v.gosse@attineos.com>
2018-10-01 08:44:40 +0000

    Merge branch 'feature/poc-native' into 'develop'
    POC native
    
    Closes #38
    
    See merge request attineos/atti-components!63

--- packages/atti-components-2/src/helpers/Typography/getFontStyle.js
+++ packages/atti-components-2/src/helpers/Typography/getFontStyle.js
@@ -3,11 +3,10 @@ import { get } from 'lodash'
 
 const getFontStyle = fontType => css`
   color: ${({ theme }) => get(theme, `components.${fontType}.colors.text`)};
-  font-family: ${({ theme }) => get(theme, `components.${fontType}.fonts.fontFamily`)};
   font-size: ${({ theme }) => get(theme, `components.${fontType}.fonts.fontSize`)};
-  font-weight: ${({ theme }) => get(theme, `components.${fontType}.fonts.fontWeight`)};
+  font-family: ${({ theme }) => get(theme, `components.${fontType}.fonts.fontFamily`)};
   line-height: ${({ theme }) => get(theme, `components.${fontType}.fonts.lineHeight`)};
-  text-align: ${({ textAlign }) => textAlign};
+  text-align: ${({ textAlign }) => textAlign || 'auto'};
 `
 
 export default getFontStyle

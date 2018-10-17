c84ce3f288c1a1440ad37f4246dd64a9515e3338
Victor Gosse <v.gosse@attineos.com>
2018-10-17 09:05:03 +0000

    Merge branch 'feature/11_input_file' into 'develop'
    Feature 11 : input file
    
    Closes #11
    
    See merge request attineos/atti-components!67

---
+++ packages/atti-components-2/src/components/InputFile/theme/index.js
@@ -0,0 +1,13 @@
+export default ({ colors, fonts, spaces }) => ({
+  colors: {
+    text: colors.darkSnow,
+    background: colors.secondary,
+  },
+  spaces: {
+    paddingTopBottom: spaces.s4,
+    paddingLeftRight: spaces.s14,
+  },
+  fonts: {
+    fontFamily: fonts.global.fontFamilyPrimary,
+  },
+})

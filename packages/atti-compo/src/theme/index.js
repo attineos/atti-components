157345593eb04a144c9a679b94ce16bb9c909ebe
Valulz <vdesportes@gmail.com>
2018-10-28 11:23:55 +0000

    Merge branch 'feature/#27_add_grid_component' into 'develop'
    feature #27: Added grid component
    
    See merge request attineos/atti-components!19

--- packages/atti-compo/src/theme/index.js
+++ packages/atti-compo/src/theme/index.js
@@ -4,17 +4,21 @@ import colors from './colors'
 import componentsFactory from './componentsFactory'
 import fonts from './fonts'
 import forms from './forms'
+import layout from './layout'
 import opacities from './opacities'
 import sizes from './sizes'
 import spaces from './spaces'
+import breakpoints from './breakpoints'
 
 export default {
+  breakpoints,
   animations,
   borders,
   colors,
   componentsFactory,
   fonts,
   forms,
+  layout,
   opacities,
   sizes,
   spaces,

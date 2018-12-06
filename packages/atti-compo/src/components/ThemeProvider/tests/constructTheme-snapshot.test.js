6e287ea1f65157bbf6a9cb6ad2eddf871ea2454a
Victor Gosse <v.gosse@attineos.com>
2018-12-06 06:35:06 +0000

    Merge branch 'feature/91-evolution-of-the-progressbar-component' into 'develop'
    Feature #91 : Evolution of the ProgressBar component
    
    See merge request attineos/atti-components!85

--- packages/atti-compo/src/components/ThemeProvider/tests/constructTheme-snapshot.test.js
+++ packages/atti-compo/src/components/ThemeProvider/tests/constructTheme-snapshot.test.js
@@ -1,16 +1,16 @@
 /* This file was generated automatically, please do not write in it. */
 import React from 'react';
-import ProgressBar from '..';
+import constructTheme from '../helpers/constructTheme';
 import Wrapper from 'styleguide/Wrapper'
 import renderer from 'react-test-renderer';
 
 const mock = () => {}
 
-it('ProgressBar renders correctly', () => {
+it('constructTheme renders correctly', () => {
   const tree = renderer
     .create(
       <Wrapper>
-        <ProgressBar className="Atticomponents" end={42} fillingOrientation={null} fillingSpeed={42} percentageLabel={true} showLabel={true} start={42} step={42} value={42} ></ProgressBar>
+        <constructTheme ></constructTheme>
       </Wrapper>
       )
     .toJSON();

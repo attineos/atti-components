6822390f0b1b56bcfc840034d999b5174a3f485d
Valentin Letourneur <v.letourneur@attineos.com>
2019-02-19 18:07:23 +0100

    feature #2: add breadcrumb custom rendering points

--- src/components/Breadcrumb/index.js
+++ src/components/Breadcrumb/index.js
@@ -2,7 +2,8 @@
 import React, { PureComponent } from 'react'
 import styled from 'styled-components'
 import PropTypes from 'prop-types'
-import { map } from 'lodash'
+import { isNull, map, isFunction, isUndefined } from 'lodash'
+import chain from 'helpers/chain'
 
 import { ElementsType } from './types'
 import BreadcrumbElement from './components/BreadcrumbElement'
@@ -14,15 +15,35 @@ import StyledBreadcrumb from './styles/StyledBreadcrumb'
  * A breadcrumb component.
  */
 class Breadcrumb extends PureComponent {
+  renderElement = element => {
+    const { renderElement } = this.props
+
+    return chain(
+      // We assume a valid result as soon as we got a non null element
+      element => !isNull(element) && !isUndefined(element),
+      // Most precise, we got a custom render function for THAT element
+      () =>
+        element && element.render && isFunction(element.render) ? element.render(element) : null,
+      // A little less precise, we got a custom render function for each item
+      () => (renderElement && isFunction(renderElement) ? renderElement(element) : null),
+      // Least precise, neither other function returned something, we default to some hand made rendering
+      () => (
+        <React.Fragment>
+          {element.url && <BreadcrumbElement element={element} />}
+          {!element.url && <CurrentElement>{element.name}</CurrentElement>}
+        </React.Fragment>
+      ),
+    )
+  }
+
   render() {
     const { className, elements, separator } = this.props
 
     return (
       <StyledBreadcrumb className={className}>
-        {map(elements, (element, index) => (
+        {map(elements, (element, index, elements) => (
           <React.Fragment key={element.name}>
-            {element.url && <BreadcrumbElement element={element} />}
-            {!element.url && <CurrentElement>{element.name}</CurrentElement>}
+            {this.renderElement(element)}
             {index + 1 < elements.length && <BreadcrumbSeparator>{separator}</BreadcrumbSeparator>}
           </React.Fragment>
         ))}
@@ -34,6 +55,7 @@ class Breadcrumb extends PureComponent {
 Breadcrumb.defaultProps = {
   className: '',
   separator: ' / ',
+  renderElement: null,
 }
 
 Breadcrumb.propTypes = {
@@ -50,6 +72,10 @@ Breadcrumb.propTypes = {
    * Separator character to put between links.
    */
   separator: PropTypes.string,
+  /**
+   * The function to use for rendering of each element
+   */
+  renderElement: PropTypes.func,
 }
 
 export default styled(Breadcrumb)``

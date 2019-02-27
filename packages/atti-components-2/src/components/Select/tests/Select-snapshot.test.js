2cd9e076925f4e48af8b2edc0ebbc4aed5e62740
Valentin Letourneur <atrakeur@users.noreply.github.com>
2019-02-27 07:53:39 +0100

    Merge pull request #36 from attineos/feature/typographies
    Feature #32: Gather all typography components

--- packages/atti-components-2/src/components/Select/tests/Select-snapshot.test.js
+++ packages/atti-components-2/src/components/Select/tests/Select-snapshot.test.js
@@ -10,7 +10,7 @@ it('Select renders correctly', () => {
   const tree = renderer
     .create(
       <Wrapper>
-        <Select className="Atticomponents" id="Atticomponents" isMulti={true} name="Atticomponents" onChange={mock} options={new Array()} size={42} sort={null} value="Atticomponents" defaultValue="Atticomponents" ></Select>
+        <Select className="Atticomponents" id="Atticomponents" isMulti={true} name="Atticomponents" onChange={mock} options={new Array()} size={42} value="Atticomponents" defaultValue="Atticomponents" ></Select>
       </Wrapper>
       )
     .toJSON();

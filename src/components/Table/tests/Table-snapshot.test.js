aa91ce86ecb9455291cb9aaf2728ab1fafc83f58
Victor Gosse <v.gosse@attineos.com>
2019-03-12 08:04:28 +0100

    Feature #4: - create a `cellItem` helper - fix warning on breadcrumb when running styleguidist

---
+++ src/components/Table/tests/Table-snapshot.test.js
@@ -0,0 +1,19 @@
+/* This file was generated automatically, please do not write in it. */
+import React from 'react';
+import Table from '..';
+import Wrapper from 'styleguide/Wrapper'
+import renderer from 'react-test-renderer';
+
+const mock = () => {}
+
+it('Table renders correctly', () => {
+  const tree = renderer
+    .create(
+      <Wrapper>
+        <Table className="Atticomponents" cols={true} name="Atticomponents" label="Atticomponents" elements={true} renderCell={mock} renderHeaderCell={mock} renderHeaderLine={mock} renderLine={mock} renderTable={mock} ></Table>
+      </Wrapper>
+      )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
+    

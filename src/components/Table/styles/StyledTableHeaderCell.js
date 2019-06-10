629d2f77e5b2a9e9a436980e344d3e5ce0066efe
Valentin Letourneur <v.letourneur@attineos.com>
2019-06-10 11:27:05 +0200

    feature: Improve table style

--- src/components/Table/styles/StyledTableHeaderCell.js
+++ src/components/Table/styles/StyledTableHeaderCell.js
@@ -1,5 +1,8 @@
 import styled from 'styled-components'
 
-const StyledTableHeaderCell = styled.th``
+const StyledTableHeaderCell = styled.th`
+  border-bottom: ${({ theme }) => theme.components.table.borders.borderWidth} solid
+    ${({ theme }) => theme.components.table.colors.border};
+`
 
 export default StyledTableHeaderCell

3cab1a09d213ef02c19599606c79913a41b2cedc
Victor G <2121948+akaala@users.noreply.github.com>
2019-06-18 08:28:51 +0200

    Merge pull request #104 from attineos/bugfix/ignore_css_snapshots
    Bugfix 103: Ignore `styled.css` elements in snapshots script

--- packages/atti-compo/bin/generateSnapshots/extractor.js
+++ packages/atti-compo/bin/generateSnapshots/extractor.js
@@ -1,3 +1,4 @@
+import { size } from 'lodash'
 /*
  * Returns an array of strings containing every props.
  */
@@ -17,3 +18,12 @@ export const extractValue = prop => prop.match(/PropTypes.[\S]+/g)
  * Returns children prop's value.
  */
 export const extractChildrenValue = rawValue => rawValue.match(/[^{"}]+/g)
+
+/*
+ * Returns the number of components & styled-components in the file.
+ */
+export const getNumberOfComponents = content => {
+  const classes = content.match(/extends (Component|PureComponent)/g)
+  const styled = content.match(/=\s?styled/g)
+  return size(classes) + size(styled)
+}

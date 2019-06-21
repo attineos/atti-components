36beab0e8898fabca8350250dc17662ea2034caa
Victor G <2121948+akaala@users.noreply.github.com>
2019-06-21 11:24:34 +0200

    Merge pull request #107 from attineos/tech/upgrade_dependencies
    Tech/upgrade dependencies

--- packages/atti-components-2/src/components/Modal/styles/TitleContainer.js
+++ packages/atti-components-2/src/components/Modal/styles/TitleContainer.js
@@ -2,12 +2,8 @@ import styled from 'styled-components'
 
 const TitleContainer = styled('div')`
   ${({ theme }) => `
-    padding: ${theme.components.modal.spaces.paddingHeightContent} ${
-    theme.components.modal.spaces.paddingWidthContent
-  };
-    border-bottom: ${theme.components.modal.borders.titleBottomWidth} solid ${
-    theme.components.modal.borders.titleBottomColor
-  };
+    padding: ${theme.components.modal.spaces.paddingHeightContent} ${theme.components.modal.spaces.paddingWidthContent};
+    border-bottom: ${theme.components.modal.borders.titleBottomWidth} solid ${theme.components.modal.borders.titleBottomColor};
   `}
   display: flex;
   justify-content: space-between;

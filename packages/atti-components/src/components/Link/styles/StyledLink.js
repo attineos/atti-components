4d32f2000dac5db2ade6af6eee301f4587836515
Victor Gosse <vgosse@mailjet.com>
2020-02-13 08:07:17 +0100

    Add visited style on Link + change a bit the default design

--- packages/atti-components/src/components/Link/styles/StyledLink.js
+++ packages/atti-components/src/components/Link/styles/StyledLink.js
@@ -1,19 +1,20 @@
-import styled, { css } from 'styled-components'
+import styled from 'styled-components'
 import PropTypes from 'prop-types'
 import { Text } from '../../Typographies'
 
-const colors = css`
-  color: ${({ theme }) => theme.components.link.colors.text};
-`
-
 const StyledLink = styled(Text.withComponent('a'))`
-  ${colors};
+  color: ${({ theme }) => theme.components.link.colors.text};
   text-decoration: none;
   border-bottom: ${({ theme }) => theme.components.link.borders.width} solid
     ${({ theme }) => theme.components.link.colors.border};
 
   &:hover {
+    color: ${({ theme }) => theme.components.link.colors.textHover};
     border-color: ${({ theme }) => theme.components.link.colors.borderHover};
+  }
+
+  &:visited {
+    color: ${({ theme }) => theme.components.link.colors.textVisited};
   }
 `
 
